446228adac106775e6f78f3d2d1ae298812649e7
Ankorkish <58787660+Ankorkish@users.noreply.github.com>
2019-12-13 09:40:38 +0200

    0818.py

---
+++ Python works/ACMP Ankorkish/0818.py
@@ -0,0 +1,4 @@
+a=int(input())
+mas=list(map(int,input().split()))
+c+=(f,for f in mas)
+print(c)
--- Python works/мои задумки (пусто)/chans.py
+++ Python works/мои задумки (пусто)/chans.py
@@ -1,4 +1,4 @@
-a=0.25
-for k in range (100):
-    a*=0.25
+a = 0.25
+for k in range(100):
+    a *= 0.25
 print(a)
