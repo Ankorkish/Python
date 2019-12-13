26725e5baa7750bf11792ddff668a602aea623eb
Ankorkish <58787660+Ankorkish@users.noreply.github.com>
2019-12-13 21:26:12 +0200

    0086.py

--- Python works/ACMP Ankorkish/0331.py
+++ Python works/ACMP Ankorkish/0331.py
@@ -1,8 +1,15 @@
-class time():
-    chas="none"
-    minuts="none"
-    def proverka(self):
-a=time()
-prochee=time()
-a.chas,a.minuts=map(int,input().split(:))
-prochee.chas,prochee.minuts=map(int,input().split())
+a,b=map(int,input().split(":"))
+c,d=map(int,input().split())
+per=b+d
+per2=a+c
+if per>=60:
+    per%=60
+    per2+=1
+if per2%24<10:
+    print("0"+str(per2%24)+":",end="")
+else:
+    print(str(per2%24) + ":", end="")
+if per<10:
+    print("0"+str(per))
+else:
+    print(str(per))
